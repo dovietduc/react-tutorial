6a16862f73c85d278d1710c54f89c40671483273
duc do <tvietduc122@gmail.com>
2022-06-11 10:49:18 +0700

    export default explain

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import User from './component/User';
+
+console.log(User(1, 2));
+
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Word</h1>
+      <User>dajjad</User>
     </div>
   );
 }
---
+++ src/component/User.js
@@ -0,0 +1,7 @@
+
+function User(a, b) {
+    return a + b;
+}
+
+
+export default User;
