c9c7241b018ad1d90a18b602bd1430ed50f43aac
duc do <tvietduc122@gmail.com>
2022-06-17 11:15:22 +0700

    setting attribute

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import Counter from './component/Counter';
 
 function App() {
+
+  const urlImage = "https://cdn.urldecoder.org/assets/images/url-fb.png";
+
   return (
     <div className="App">
+      <img className="image_feature" src={urlImage}/>
       <h1>Hello Word</h1>
+      <Counter/>
     </div>
   );
 }
 
 export default App;
+
+// reserve keyword
---
+++ src/component/Counter.js
@@ -0,0 +1,14 @@
+
+
+function Counter() {
+    const styles = {
+        fontSize: "20px",
+        fontWeight: "bold"
+    }
+    return (
+        <button style={styles}>Create</button>
+    )
+
+}
+
+export default Counter;
