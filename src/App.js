6f0ac0b86a10c8bcd4f8a51e578e0188773e47ce
duc do <tvietduc122@gmail.com>
2022-06-22 15:15:25 +0700

    render_class_dynamic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [counter, setCounter] = useState(1);
+
+  const formatClass = function() {
+    let clasCommon = "badge p-3";
+    if(counter == 0) {
+      clasCommon += " badge-warning";
+    } else {
+      clasCommon += " badge-primary";
+    }
+
+    return clasCommon;
+  }
+
   return (
     <div className="App">
       <h1>Hello Word</h1>
+      <span class={formatClass()}>Primary</span>
     </div>
   );
+
 }
 
 export default App;
