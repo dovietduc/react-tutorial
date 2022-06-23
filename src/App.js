47a0f0d8dca81ce243bb99d29da11bc2fbf649e3
duc do <tvietduc122@gmail.com>
2022-06-23 23:20:02 +0700

    render list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello Word</h1>
-    </div>
-  );
+
+	const [tags, setTags] = useState(['tag1', 'tag2', 'tag3']);
+
+	const renderTags = function() {
+		const listTags = tags.map(function(tag){
+			return (
+				<li>{tag}</li>
+			)
+		});
+
+		return listTags;
+	}
+
+    return (
+        <div className="App">
+			<ul>
+				{
+					tags.map(function(tag){
+						return (
+							<li key={tag}>{tag}</li>
+						)
+					})
+				}
+			</ul>
+            <h1>Hello Word</h1>
+        </div>
+    );
 }
 
 export default App;
