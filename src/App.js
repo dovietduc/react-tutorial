36f143dd3035dc868109e599a55fe49ae404dc8e
duc do <tvietduc122@gmail.com>
2022-06-28 22:22:42 +0700

    arrow function map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello Word</h1>
-    </div>
-  );
+
+	const [tags, setTags] = useState(['tag1', 'tag2', 'tag3']);
+
+    return (
+        <div className="App">
+			<ul>
+				{
+					tags.map(
+						tag => {
+							return (
+								<p>
+									<li>{tag}</li>
+								</p>
+							)
+						}
+					)
+				}
+			</ul>
+            <h1>Hello Word</h1>
+        </div>
+    );
 }
 
 export default App;
