f1b4446ca2d7486aa9a6f6b68e982becab3e20b4
duc do <tvietduc122@gmail.com>
2022-07-16 07:39:49 +0700

    useefect end

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import {useEffect, useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello Word</h1>
-    </div>
-  );
+
+	const [post, setPost] = useState({});
+
+	useEffect(() => {
+
+		console.log('useefeect running');
+		fetch('https://jsonplaceholder.typicode.com/todos/1')
+			.then(response => response.json())
+			.then(json => {
+				setPost(json);
+			})
+	}, [])
+
+    return (
+        <div className="App">
+            <div>
+				<h1>{post.id}</h1>
+				<p>{post.title}</p>
+			</div>
+        </div>
+    );
+
 }
 
 export default App;
