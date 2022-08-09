5e7abb9df3bcdee27a8e79d729ccdfc2380bf51f
duc do <tvietduc122@gmail.com>
2022-08-09 18:42:04 +0700

    tab render

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,14 +1,37 @@
+import {useState, useEffect} from 'react';
+
 import TabNavigation from "./TabNavigation";
 import TabContent from "./TabContent";
-import {useState} from 'react';
+
+
+
+let i = 0;
+
+const currentState = {current: ''};
 
 function Tab() {
+    i++;
 
     const [activeTab, setActiveTab] = useState('react');
 
     const handleClickActiveTab = (idTab) => {
         setActiveTab(idTab);
     }
+
+    useEffect(() => {
+
+
+        // luôn luôn chạy khi click sang tab khác và lần đầu render
+
+        console.group();
+
+        console.log('state before change', currentState.current);
+        currentState.current = activeTab;
+        console.log('state after change', currentState.current);
+        console.groupEnd();
+
+
+    }, [activeTab])
 
     return (
         <div className="container">
@@ -31,4 +54,6 @@ export default Tab;
 
 // 1. data truyen tu con len
 // 2. thay doi state o trong tab cha theo gia tri con truyen len khi click
+
+// useEffect
 
